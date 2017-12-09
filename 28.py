30bea3abce2aa5109359e655c249d2212c4099f7
[Daria] <33488966+DomaninaDaria@users.noreply.github.com>
2017-12-09 16:50:27 +0200

    exercise 28

--- 28.py
+++ 28.py
@@ -1,24 +1,20 @@
 
 
-lst1 = ["a", "b", 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-print(lst1)
+encode_list = ["a", "b", 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+               's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+print(encode_list)
 string = input("Input your string:")
 string_ = string.lower()
 print(string_)
 
 
 def encode(str_to_encode):
-    str1 = ""
+    result = ""
     for i in range(len(str_to_encode)):
-        for j in range(len(lst1)):
-            if str_to_encode[i] == str(lst1[j]):
-                if (j+5) < (len(lst1) - 1):
-                    str1 += str(lst1[j+5])
-                else:
-                    p = len(lst1)-j
-                    str1 += str(lst1[5 - p])
-    return str1
+        for j in range(len(encode_list)):
+            if str_to_encode[i] == encode_list[j]:
+                result += str(encode_list[(j + 5) % len(encode_list)])
+    return result
 
 
 print("Encoded string is:", encode(string_))
