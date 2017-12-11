613520b9c8e8859c3e54c47ec516b6b188a630c3
[Daria] <33488966+DomaninaDaria@users.noreply.github.com>
2017-12-11 15:43:31 +0200

    замечания учтены, также добавлена проверка на пунктуацию)

--- 27-.py
+++ 27-.py
@@ -1,20 +1,32 @@
+import string
 import random
-s = "In any business the most important thing is to start no one has been able to succeed planning"
+s = "In any business the most important thing is to start, no one has been able to succeed planning!"
 count = 0
 
 
 def pemrtuate(text):
     lst = text.split(" ")
     print(lst)
+    punctuation = string.punctuation
+    print(punctuation)
+    f = True
     for i in range(len(lst)):
         lst2 = [] * len(lst[i])
         index = (len(lst[i])-1)
-        for j in range(1, len(lst[i])-1):
-            lst2 += lst[i][j]
+        for j in range(len(punctuation)):
+            if lst[i][index] == punctuation[j]:
+                f = False
+        if(f):
+            lst2 += lst[i][1:-1]
+        else:
+            lst2 += lst[i][1:-2]
         random.shuffle(lst2)
         lst2.insert(0, lst[i][0])
-        lst2.append(lst[i][index])
-        print(lst2)
+        if(f):
+            lst2.append(lst[i][index])
+        else:
+            lst2.append(lst[i][index - 1])
+            lst2.append(lst[i][index])
         lst[i] = "".join(lst2)
     print(" ".join(lst))
 
