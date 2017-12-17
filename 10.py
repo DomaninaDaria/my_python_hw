87e2e3b55a74d9da2a3d541cc994e1ffcd536abd
[Daria] <33488966+DomaninaDaria@users.noreply.github.com>
2017-12-18 00:56:44 +0200

    test 10(corrected)

--- 10.py
+++ 10.py
@@ -1,33 +1,33 @@
-N = int(input("Input value of  row:"))
-M = int(input("Input value of column: "))
-massif = [[0] * M for i in range(N)]
+N = int(input("Input value of  rows:"))
+M = int(input("Input value of columns: "))
+matrix = [[0] * M for i in range(N)]
 for i in range(N):
     for j in range(M):
         print("Input element in position ")
-        massif[i][j] = int(input())
+        matrix[i][j] = int(input())
 for i in range(N):
     for j in range(M):
-        print("\t",massif[i][j], end="")
+        print("\t", matrix[i][j], end="")
     print("")
 min_in_row = 0
 min_index_i = 0
 min_index_j = 0
-sedl = 0
+saddle = 0
 for i in range(N):
-    min_in_row = massif[i][0]
+    min_in_row = matrix[i][0]
     for j in range(M-1):
         min_index_i = i
-        if massif[i][j+1] < min_in_row:
-            min_in_row = massif[i][j+1]
+        if matrix[i][j+1] < min_in_row:
+            min_in_row = matrix[i][j + 1]
             min_index_j = j+1
-    f = True
+    is_max_in_column = True
     for k in range(N):
-        if massif[min_index_i][min_index_j] < massif[k][min_index_j]:
-            f = False
+        if matrix[min_index_i][min_index_j] < matrix[k][min_index_j]:
+            is_max_in_column = False
             break
-    if(f):
-        sedl = massif[min_index_i][min_index_j]
-        print(sedl)
+    if(is_max_in_column):
+        saddle = matrix[min_index_i][min_index_j]
+        print(saddle)
 
 
 
