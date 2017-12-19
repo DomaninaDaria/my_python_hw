1f8a154d604a85f30764409d5d89e699a9f6a338
[Daria] <33488966+DomaninaDaria@users.noreply.github.com>
2017-12-19 22:08:44 +0200

    12

--- test-12.py
+++ test-12.py
@@ -8,17 +8,17 @@ while len(result) != 15:
     new_values_are_not_in_used_values_list = True
     index = 2 + k
     k += 1
-    number1 = random.choice(list_of_values)
+    number_1 = random.choice(list_of_values)
     number2 = random.choice(list_of_values)
-    list_of_used_values += [number1, number2]
+    list_of_used_values += [number_1, number2]
     for j in range(len(list_of_used_values) - index):
-        if number1 == list_of_used_values[2*j] and number2 == list_of_used_values[2*j + 1] \
-                or number1 == list_of_used_values[2*j + 1] and number2 == list_of_used_values[2*j]:
+        if number_1 == list_of_used_values[2*j] and number2 == list_of_used_values[2*j + 1] \
+                or number_1 == list_of_used_values[2*j + 1] and number2 == list_of_used_values[2*j]:
             new_values_are_not_in_used_values_list = False
             break
 
     if new_values_are_not_in_used_values_list:
-        result.append(number1*number2)
-        print("%d * %d = %d " % (number1, number2, number1 * number2))
+        result.append(number_1 * number2)
+        print("%d * %d = %d " % (number_1, number2, number_1 * number2))
 
 
